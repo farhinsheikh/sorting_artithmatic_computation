cb33c1fe41e3c1219b8fb2e0510fd28f764ccae2
Farhin Sheikh <farhinsheikh2015@gmail.com>
2020-05-27 12:06:23 +0530

    compute a+b*c

--- sort_Arithmatic_Computation.sh
+++ sort_Arithmatic_Computation.sh
@@ -4,3 +4,7 @@ echo "welcome to the arithmatic computation program"
 read -p "Enter a: " a
 read -p "Enter b: " b
 read -p "Enter c: " c
+
+result1=$(( ($a+$b) * $c))
+computation[result1]=$result1
+echo "a+b*c = $result1"
