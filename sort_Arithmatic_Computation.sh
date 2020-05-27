223e0f0a5ecb87c107d45922df0059d20b06fb1a
Farhin Sheikh <farhinsheikh2015@gmail.com>
2020-05-27 12:50:54 +0530

    stored the values in dictionary

--- sort_Arithmatic_Computation.sh
+++ sort_Arithmatic_Computation.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -x
 echo "welcome to the arithmatic computation program"
 
+declare -A computation
+
 read -p "Enter a: " a
 read -p "Enter b: " b
 read -p "Enter c: " c
@@ -20,3 +22,7 @@ echo "c+a/b = $result3"
 result4=$(( ($a%$b) + $c))
 computation[result4]=$result4
 echo "a%b+c = $result4"
+
+count=0
+echo "Stored in dictionary"
+
