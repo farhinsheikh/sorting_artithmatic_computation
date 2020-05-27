347970a032deb533a1e8eb86b7dce540c7ad34d8
Farhin Sheikh <farhinsheikh2015@gmail.com>
2020-05-27 12:34:31 +0530

    compute a%b+c

--- sort_Arithmatic_Computation.sh
+++ sort_Arithmatic_Computation.sh
@@ -16,3 +16,7 @@ echo "a*b+c = $result2"
 result3=$(( ($c+$a) / $b))
 computation[result3]=$result3
 echo "c+a/b = $result3"
+
+result4=$(( ($a%$b) + $c))
+computation[result4]=$result4
+echo "a%b+c = $result4"
